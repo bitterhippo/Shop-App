855ac0babdc869136c93cea3a83073331050b35b
Noah <bitterhippo@gmail.com>
2021-06-13 18:03:20 +0900

    Update ProductDetailScreen.js

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { ScrollView, View, Text, Image, StyleSheet, Button } from 'react-native';
+import { useSelector } from 'react-redux';
 
 const ProductDetailScreen = props => {
+  const productId = props.navigation.getParam('productId');
+  const selectedProduct = useSelector(state => state.products.availableProducts.find(prod => prod.id === productId))
   return (
     <View>
-
+      <Text>{selectedProduct.title}</Text>
     </View>
   )
 };
