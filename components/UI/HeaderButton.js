475af219075f2fba1093734e5db4d2572f7ffbd2
Noah <bitterhippo@gmail.com>
2021-06-16 20:58:26 +0900

    Update HeaderButton.js

--- components/UI/HeaderButton.js
+++ components/UI/HeaderButton.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { TouchableOpacity } from 'react-native';
 import { FiShoppingCart } from 'react-icons/fi';
 
-
-import Colors from '../../constants/Colors';
-
 const CustomHeaderButton = props => {
   return (
     <TouchableOpacity onPress={() => {console.log('lol')}}>
